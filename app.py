646b168cc51ad2a95d326457042b4a0c1aaff035
David Tardy <davidctardy@gmail.com>
2019-05-18 20:55:42 -0500

    Planning Notes + zip import

---
+++ app.py
@@ -0,0 +1,26 @@
+import constants.py 
+
+if __name__ == '__main__':
+
+#import constants.py
+#catch all errors and raise exceptions
+#all execution logic should be wrapped in __main__
+#clean up constants.py and create new data structure to hold cleaned data
+##convert hieght to an integer
+#experienced string == boolean True(experienced) or False(not experienced)
+#guardians string turned into list of strings.
+    #remove the 'and' fromplayers with multiple guardians
+#store each guardian in a list for that player
+#assign players to team so teams are evenly balanced by total players
+#same player cannot be assigned twice
+#balance teams also by experienced vs inexperienced players
+#create menu of available options(display team stats, which team?, quit)
+#reprompt user to go back to main menu until they quit
+#team stats:
+    #Team name
+    #total number of players
+    #player names joined together as comma seperated string, Not list object.
+    #total number of experienced players
+    #total number of inexperienced players
+    #average hieght of players
+    #the guardian names of all players of the team joined together in comma seperated string
